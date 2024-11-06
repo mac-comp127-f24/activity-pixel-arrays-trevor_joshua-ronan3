978f4628d41c787870c37010fa89a901d571d9fd
twongvanharen <twongvan@macalester.edu>
2024-11-06 12:58:03 -0600

    ICA 11/6

--- src/ImageTransform.java
+++ src/ImageTransform.java
@@ -2,26 +2,47 @@ import java.util.Scanner;
 
 import edu.macalester.graphics.CanvasWindow;
 import edu.macalester.graphics.Image;
+import edu.macalester.graphics.Image.PixelFormat;
 
 public class ImageTransform {
 
     public static Image lighten(Image srcImage) {
         // TODO: Task 1
-
-        throw new UnsupportedOperationException("Method not yet defined");
+        float[] pixels = new float[300];
+        pixels = srcImage.toFloatArray(PixelFormat.RGB);
+        for (int i = 0; i < pixels.length; i ++ ){
+            pixels[i] = (float) (pixels[i] * 1.5);
+        }
+        Image newImg = new Image((int)srcImage.getImageWidth(),(int)srcImage.getImageHeight(), pixels, PixelFormat.RGB);
+        return newImg;
+        // float[] pixels = Image.toFloatArray(srcImage);
+        // throw new UnsupportedOperationException("Method not yet defined");
     }
 
 
     public static Image greenShift(Image srcImage) {
         // TODO: Task 2
+        float[] pixels = new float[300];
+        pixels = srcImage.toFloatArray(PixelFormat.RGB);
+        for (int i = 1;i < pixels.length; i += 3 ){
+            pixels[i] = (float) (pixels[i] + .25);
+        }
+        Image newImg = new Image((int)srcImage.getImageWidth(),(int)srcImage.getImageHeight(), pixels, PixelFormat.RGB);
+        return newImg;
 
-        throw new UnsupportedOperationException("Method not yet defined");
+        // throw new UnsupportedOperationException("Method not yet defined");
     }
 
     public static Image invert(Image srcImage) {
         // TODO: Task 3
+        byte[] pixels = srcImage.toByteArray(PixelFormat.RGB);
+        for (int i = 0; i < pixels.length; i ++ ){
+            pixels[i] = (byte) (255 - pixels[i]);
+        }
+        Image newImg = new Image((int)srcImage.getImageWidth(),(int)srcImage.getImageHeight(), pixels, PixelFormat.RGB);
+        return newImg;
 
-        throw new UnsupportedOperationException("Method not yet defined");
+        // throw new UnsupportedOperationException("Method not yet defined");
     }
 
     public static void main(String[] args) {
